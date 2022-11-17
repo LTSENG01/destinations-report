2d4fb0b337ac8ac2238fc818e065589648969078
Larry Tseng <larry.tseng.1@gmail.com>
2022-11-17 12:24:36 -0500

    Add Cross-Domain messaging for iframe resizing

--- js/index.js
+++ js/index.js
@@ -38,3 +38,14 @@ const myChart = new Chart(
 const headlines = [
 
 ]
+
+// https://www.geeksforgeeks.org/how-to-adjust-the-width-and-height-of-iframe-to-fit-with-content-in-it/
+// https://stackoverflow.com/questions/9153445/how-to-communicate-between-iframe-and-the-parent-site
+window.onload(() => {
+    const message = {
+        height: window.document.body.scrollHeight,
+        width: window.document.body.scrollWidth
+    }
+    window.postMessage(message, "https://cics.umass.edu");
+});
+
